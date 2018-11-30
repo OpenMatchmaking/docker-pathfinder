234d92f41021d0450eb07570faa4aa4ef13f94a8
Valeryi Savich <Relrin78@gmail.com>
2018-11-30 14:53:51 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,20 +10,12 @@ OPTIONS=$"
 --rabbitmq-virtual-host=${RABBITMQ_VIRTUAL_HOST}
 --rabbitmq-user=${RABBITMQ_USER}
 --rabbitmq-password=${RABBITMQ_PASSWORD}
---redis-host=${REDIS_HOST}
---redis-port=${REDIS_PORT}
---redis-password=${REDIS_PASSWORD}
---jwt-secret=${JWT_SECRET}
 --ssl-cert=${SSL_CERTIFICATE}
 --ssl-key=${SSL_KEY}
 "
 
 if [[ "${SECURED_MODE}" =~ ^(yes|Yes|YES)$ ]]; then
   OPTIONS="-s $OPTIONS"
-fi
-
-if [[ "${VALIDATE_TOKEN}" =~ ^(yes|Yes|YES)$ ]]; then
-  OPTIONS="-v $OPTIONS"
 fi
 
 ./pathfinder $OPTIONS
